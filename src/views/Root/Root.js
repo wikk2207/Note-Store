1d656ef87ef33097f7350be07e733373bda9654d
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-10-16 16:19:09 +0200

    feat(templates): add MainTemplate

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import MainTemplate from 'templates/MainTemplate';
 import Button from 'components/atoms/Button/Button';
-import Heading from 'components/atoms/Heading/Heading';
-import Input from 'components/atoms/Input/Input';
-import Paragraph from 'components/atoms/Paragraph/Paragraph';
-import GlobalStyle from 'theme/GlobalStyle';
-import { theme } from 'theme/mainTheme';
 
-function Root() {
-  return (
-    <div>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <>
-          <h1>Hello Wiktoria</h1>
-          <Button width="500px">close / safe</Button>
-          <Button secondary>Remove</Button>
-          <Heading>Hello</Heading>
-          <Heading big>Hello</Heading>
-          <Paragraph>Heloo wiktoria</Paragraph>
-          <Input />
-        </>
-      </ThemeProvider>
-    </div>
-  );
-}
+const Root = () => (
+  <MainTemplate>
+    <>
+      <h1>Hello Wiktoria</h1>
+      <Button> Close</Button>
+    </>
+  </MainTemplate>
+);
 
 export default Root;
