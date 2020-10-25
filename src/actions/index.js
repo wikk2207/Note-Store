7ae3cb94e5d48e921e732642d7399b15ee357ee2
Wiktoria <wiktoriajuliasalamon@gmail.com>
2020-10-25 22:33:15 +0100

    refactor(actions): replace all strings by variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,29 +1,10 @@
 import axios from 'axios';
 
-export const removeItem = (itemType, id) => {
-  return {
-    type: 'REMOVE_ITEM',
-    payload: {
-      itemType,
-      id,
-    },
-  };
-};
-
-export const addItem = (itemType, itemContent) => {
-  const getId = () => `_${Math.random().toString(36).substr(2, 9)}`;
-
-  return {
-    type: 'ADD_ITEM',
-    payload: {
-      itemType,
-      item: {
-        id: getId(),
-        ...itemContent,
-      },
-    },
-  };
-};
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const ADD_ITEM = 'ADD_ITEM';
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAILURE = 'AUTH_FAILURE';
 
 export const authenticate = (username, password) => (dispatch) => {
   dispatch({ type: 'AUTHENTICATE_REQUEST' });
@@ -40,4 +21,29 @@ export const authenticate = (username, password) => (dispatch) => {
       console.log(err);
       dispatch({ type: 'AUTHENTICATE_FAILURE' });
     });
+};
+
+export const removeItem = (itemType, id) => {
+  return {
+    type: REMOVE_ITEM,
+    payload: {
+      itemType,
+      id,
+    },
+  };
+};
+
+export const addItem = (itemType, itemContent) => {
+  const getId = () => `_${Math.random().toString(36).substr(2, 9)}`;
+
+  return {
+    type: ADD_ITEM,
+    payload: {
+      itemType,
+      item: {
+        id: getId(),
+        ...itemContent,
+      },
+    },
+  };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { ADD_ITEM, REMOVE_ITEM, AUTH_REQUEST, AUTH_SUCCESS, AUTH_FAILURE } from 'actions';
+
 const initialState = {
   twitters: [
     {
@@ -102,12 +104,12 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_ITEM':
+    case ADD_ITEM:
       return {
         ...state,
         [action.payload.itemType]: [...state[action.payload.itemType], action.payload.item],
       };
-    case 'REMOVE_ITEM':
+    case REMOVE_ITEM:
       return {
         ...state,
         [action.payload.itemType]: [
